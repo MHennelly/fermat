4df43e68c8ab6d5b477736f13b3c47b446f25ebd
MHennelly <michaelthehennelly@gmail.com>
2018-12-26 01:37:02 -0800

    updated script

--- script.py
+++ script.py
@@ -2,8 +2,16 @@
 from math import log
 print("NOTE: Hit Ctrl-C to interrupt the program")
 print("NOTE: Values too large for float representation will be printed in a logarithmic scale")
-c = int(input("Enter value for c : "))
-n = 3
+print("NOTE: Acceptable values for c are restricted to integers greater than or equal to 2")
+try:
+    c = int(input("Enter value for c : "))
+except ValueError:
+    print("Value error, please enter an integer")
+    exit(1)
+if c < 2:
+    print("Detected invalid integer value for c, exiting program...")
+    exit(1)
+n = 2
 try:
     while True:
         n += 1
@@ -19,9 +27,9 @@ try:
                     exit(0)
                 else:
                     try:
-                        print("a: %d, b: %d, c: %d, n: %d | a^n + b^n: %.2E | c^n: %.2E" % (a,b,c,n,test,goal))
+                        print("a: %d, b: %d, c: %d, n: %d | a^n + b^n: %.8E | c^n: %.8E" % (a,b,c,n,test,goal))
                     except OverflowError:
                         logTest = log(test)
-                        print("a: %d, b: %d, c: %d, n: %d | log(a^n + b^n): %.2E | log(c^n): %.2E" % (a,b,c,n,logTest,logGoal))
+                        print("a: %d, b: %d, c: %d, n: %d | log(a^n + b^n): %.8E | log(c^n): %.8E" % (a,b,c,n,logTest,logGoal))
 except KeyboardInterrupt:
     print("Script interrupted...")
